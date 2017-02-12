e750d0071b96cd788e627df99f7320b93ddf0f7e
Žiga Fende <zfende@gmail.com>
2017-02-12 14:44:16 +0100

    dodan int in while

--- ugani_stevilo.py
+++ ugani_stevilo.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-print ("Pozdravljeni v igri ugani skrito število!")
-skrivno_stevilo = "42"
+print ("Pozdravljeni v igri ugani skrito stevilo!")
+skrivno_stevilo = 42
 
-ugani_stevilo = raw_input("Izberite število med 1 in 100: ")
-
-if ugani_stevilo == skrivno_stevilo:
-    print ("BRAVO USPELO TI JE!")
-elif ugani_stevilo > skrivno_stevilo:
-    print ("Nižje, poskusite ponovno")
-elif ugani_stevilo < skrivno_stevilo:
-    print ("Višje, poskusite ponovno")
+while True:
+    ugani_stevilo = int(raw_input("Izberite stevilo med 1 in 100: "))
+    if ugani_stevilo == skrivno_stevilo:
+        print ("BRAVO USPELO TI JE!")
+        break
+    elif ugani_stevilo > skrivno_stevilo:
+        print ("Nizje, ugani stevilo se enkrat: ")
+    else:
+        print ("Visje, ugani stevilo se enkrat: ")
